661809ebf56aacc8a1f1ef44bfdcf9a39c23d4be
callmeizel <sanguthapa2009@gmail.com>
2025-07-09 11:44:25 +0530

    Update audio_page.js

--- audio_page.js
+++ audio_page.js
@@ -63,7 +63,7 @@ okButton.addEventListener('click', () => {
   formData.append('file', file);
 
   // Call API in parallel but display after 5s (loading bar)
-  fetch("http://127.0.0.1:8000/", {
+  fetch("http://127.0.0.1:8000/predict_audio_emotion", {
     method: "POST",
     body: formData
   })
